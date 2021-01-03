6cef7dc469b86f4865f54fbefcdbad3ad825b15c
Kassam Ali Asif <asif.k.dev@gmail.com>
2021-01-03 17:45:40 +0100

    improvement display cards vocabulary

--- src/Screens/VocabJlpt5.js
+++ src/Screens/VocabJlpt5.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const VocabJlpt5 = () => {
 
@@ -6,6 +6,7 @@ const VocabJlpt5 = () => {
     const [furigana, setFurigana] = useState('');
     const [translation, setTranslation] = useState('');
     const [duplicateFurigana, setDuplicateFurigana] = useState('');
+    const [duplicate, setDuplicate] = useState(false);
 
     useEffect(() => {
         const retrieveData = async () => {
@@ -14,60 +15,60 @@ const VocabJlpt5 = () => {
             setVocabulary(data);
         }
         retrieveData();
-    },[]) 
-    return(
+    }, [])
+    return (
         <div className="center wrap">
             {
                 vocabulary.map((value) => (
                     <div className="card cardVocab" key={value[0] + '_' + value[1]}>
-                    <div className="card-body">
-                        <div className="card-text">
-                            <div>
-                                <span data-toggle="tooltip" data-placement="top" title="Hiragana">
-                                    {value[0]}
-                                </span>
+                        <div className="card-body">
+                            <div className="card-text">
+                                <div>
+                                    <span data-toggle="tooltip" data-placement="top" title="Hiragana">
+                                        {value[0]}
+                                    </span>
+                                </div>
                             </div>
                         </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <div>
+                                    <button className="button" onClick={() => {
+                                        if (furigana !== value[1]) {
+                                            setFurigana(value[1]);
+                                        } else {
+                                            setFurigana('');
+                                        }
+                                    }}>
+                                        {furigana === value[1] ? 'Hide furigana' : 'Show furigana'}
+                                    </button>
+                                </div>
+                                {
+                                    furigana === value[1] ? <div className='furigana'>{value[1]}</div> : <div className='furigana'>Hidden</div>
+                                }
+                            </li>
+                            <li className="list-group-item">
+                                <div>
+                                    <button className="button" onClick={() => {
+                                        if (translation !== value[2]) {
+                                            setTranslation(value[2]);
+                                            setDuplicateFurigana(value[1]);
+                                        }
+                                        else {
+                                            setTranslation('');
+                                            setDuplicateFurigana('');
+                                            console.log('else');
+                                        }
+                                    }}>
+                                        {(translation === value[2] && duplicateFurigana === value[1]) ? ('Hide translation') : ('Show translation')}
+                                    </button>
+                                </div>
+                                {
+                                    (translation === value[2] && duplicateFurigana === value[1]) ? (<div className='translation'>{value[2].charAt(0).toUpperCase() + value[2].slice(1)}</div>) : (<div className='translation'>Hidden</div>)
+                                }
+                            </li>
+                        </ul>
                     </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <div>
-                                <button className="button" onClick={() => {
-                                    if (furigana !== value[1]) {
-                                        setFurigana(value[1]);
-                                    } else {
-                                        setFurigana('');          
-                                    }
-                                }}>
-                                    {furigana === value[1] ? 'Hide furigana' : 'Show furigana'}
-                                </button>
-                            </div>
-                            {
-                                furigana === value[1] ? <div className='furigana'>{value[1]}</div> : <div className='furigana'>Hidden</div>
-                            }
-                        </li>
-                        <li className="list-group-item">
-                            <div>
-                                <button className="button" onClick={() => {
-                                    if (translation !== value[2]) {
-                                        setTranslation(value[2]);
-                                        setDuplicateFurigana(value[1]);
-                                        console.log('if');
-                                    } else {
-                                        setTranslation('');
-                                        setDuplicateFurigana('');   
-                                        console.log('else');       
-                                    }
-                                }}>
-                                    {translation === value[2] && duplicateFurigana === value[1] ? 'Hide translation' : 'Show translation'}
-                                </button>
-                            </div>
-                            {
-                                translation === value[2] && duplicateFurigana === value[1] ? <div className='translation'>{value[2].charAt(0).toUpperCase() + value[2].slice(1)}</div> : <div className='translation'>Hidden</div>
-                            }
-                        </li>
-                    </ul>
-                </div>
                 ))
             }
         </div>
